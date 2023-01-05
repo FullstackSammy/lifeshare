78d60889f48f4571b49e4d39e3b50c0b6fada237
Samuel Näsström <nasstromsamuel@icloud.com>
2023-01-05 20:04:17 +0000

    added navbar structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Button variant="info">Click Me</Button>
+      <NavBar/>
     </div>
   );
 }
