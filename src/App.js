88b91ffececba69ddcb449412f50b5ffd3d2e055
Samuel Näsström <nasstromsamuel@icloud.com>
2023-01-06 18:31:55 +0000

    installed react-router and set the routes for the navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import {Router, Switch} from 'react-router-dom'
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 
 
@@ -7,6 +10,14 @@ function App() {
   return (
     <div className={styles.App}>
       <NavBar/>
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>}/>
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>}/>
+          <Route exact path='/signup' render={() => <h1>Sign Up</h1>}/>
+          <Route render={() => <p>Page not found!</p>}/>
+        </Switch>
+      </Container>
     </div>
   );
 }
