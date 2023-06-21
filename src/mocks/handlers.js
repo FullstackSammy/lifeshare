0e00f03ac69c0f2d28729b9da007908421d3d394
Samuel Näsström <nasstromsamuel@icloud.com>
2023-06-21 19:18:10 +0000

    changed database url

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,6 +1,6 @@
 // import { rest } from "msw";
 
-// const baseURL = 'https://drf-api-sam.herokuapp.com/';
+// const baseURL = 'https://drf-api-sam1.onrender.com/';
 
 // export const handlers = [
 //     rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
